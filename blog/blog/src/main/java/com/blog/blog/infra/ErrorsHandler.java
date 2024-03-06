8345f0e29cf05ae5b754e904d2a1649bb3ccfefb
Felipe Tavares <https://github.com/FelipeLTavares>
2024-03-06 16:00:22 -0300

    Erros de validação

--- blog/blog/src/main/java/com/blog/blog/infra/ErrorsHandler.java
+++ blog/blog/src/main/java/com/blog/blog/infra/ErrorsHandler.java
@@ -1,6 +1,8 @@
 package com.blog.blog.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,9 +11,22 @@ import jakarta.persistence.EntityNotFoundException;
 @RestControllerAdvice
 public class ErrorsHandler {
 
+    private record FieldsValidationError(String campo, String mensagem) {
+        public FieldsValidationError( FieldError errors ) {
+            this(errors.getField(), errors.getDefaultMessage());
+        }
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleNotFound() {
+    public ResponseEntity<?> handleError404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleError400(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(FieldsValidationError::new).toList());
     }
 
 }
